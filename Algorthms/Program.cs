86bc6c3215eb57e9ed9a73639ede2e4fbea057e5
ImanliOmer <100156424+ImanliOmer@users.noreply.github.com>
2023-01-13 18:59:26 +0400

    Add files via upload
    alll files uploaded

---
+++ Algorthms/Program.cs
@@ -0,0 +1,54 @@
+﻿
+//int number = 76;
+//int rem = 0;
+//int sum = 0;
+//while (number > 0)
+//{
+//    rem = number % 10;
+//    sum = sum + rem;
+//    number = number / 10;
+
+//}
+//if (sum % 2 == 0)
+//{
+//    Console.WriteLine("eded cutdur");
+//}
+//else
+//{
+//    Console.WriteLine("eded tekdir");
+//}
+
+
+//int number = 100;
+//int sum = 0;
+
+//for (int i = 100; i > 0; i--)
+//{
+//    if (i % 13 == 0)
+//    {
+//        Console.WriteLine(i);
+//        sum = sum + i;
+//    }
+//}
+//Console.WriteLine(sum);
+
+
+
+
+int number = 987;
+int rem = 0;
+int min = number % 10;
+while (number > 0)
+{
+    rem = number % 10;
+    if (rem <= min)
+    {
+        min = rem;
+
+    }
+    number /= 10;
+}
+
+Console.WriteLine(min);
+
+
